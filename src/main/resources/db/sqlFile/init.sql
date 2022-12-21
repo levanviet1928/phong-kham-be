f006f2e207dde666faa0ed12cb6542b3323b4de9
giangnt123 <giangnt123@viettel.com.vn>
2022-12-21 17:40:25 +0700

    first commit

---
+++ src/main/resources/db/sqlFile/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE his.account (
+    account_id varchar(36) NOT NULL
+)
+    ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
---
+++ src/test/java/com/team2/his/HisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
