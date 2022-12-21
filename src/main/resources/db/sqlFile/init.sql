7a46f94b816b8fc0224b0d10bffb8af3b94fca79
giangnt123 <giangnt123@viettel.com.vn>
2022-12-21 17:52:04 +0700

    create folder project

--- src/main/resources/db/sqlFile/init.sql
+++ src/main/resources/db/sqlFile/init.sql
@@ -1,6 +1,9 @@
-CREATE TABLE his.account (
-    account_id varchar(36) NOT NULL
+CREATE TABLE account (
+                             account_id varchar(36) NOT NULL,
+                             user_name varchar(100) NOT NULL,
+                             password varchar(100) NOT NULL,
+                             is_delete INT NULL COMMENT '1 là đã bị xóa, 0 chưa bị xóa'
 )
     ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
